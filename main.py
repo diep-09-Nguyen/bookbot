f017dcd8f441a20d7b5e539a30084c21ce434d96
DiepDN94 <diep.d.nguyen94@gmail.com>
2025-03-12 00:30:49 -0500

    Update so that bookbot can be used with any book

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from stats import get_num_words
 from stats import get_num_characters
 from stats import print_report
+import sys
 
 def main():
-    print("============ BOOKBOT ============")
+
+    if len(sys.argv) <= 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     
-    path_to_file = "books/frankenstein.txt"
+    print("============ BOOKBOT ============")
+    path_to_file = sys.argv[1]
     print(f"Analyzing book found at {path_to_file}...")
 
     print("----------- Word Count ----------")
