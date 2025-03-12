469b6e9809eb589d0652dcade0e734b44d08b9ed
DiepDN94 <diep.d.nguyen94@gmail.com>
2025-03-12 00:13:07 -0500

    Added formatting for bookbot

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from stats import get_num_words
+from stats import get_num_characters
+from stats import print_report
 
 def main():
-    number_of_words = get_num_words("books/frankenstein.txt")
-    print(f"{number_of_words} words found in the document")
+    print("============ BOOKBOT ============")
+    
+    path_to_file = "books/frankenstein.txt"
+    print(f"Analyzing book found at {path_to_file}...")
+
+    print("----------- Word Count ----------")
+    number_of_words = get_num_words(path_to_file)
+    print(f"Found {number_of_words} total words")
+
+    print("--------- Character Count -------" )
+    character_dict = get_num_characters(path_to_file)
+    print_report(character_dict)
 
 main()
